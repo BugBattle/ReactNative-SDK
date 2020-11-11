a60cf2496f12e299636b4843f0a780e11e004d82
Lukas Böhler <lukas@boehlerbrothers.com>
2020-11-11 19:15:30 +0100

    Added types.

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,18 @@
-export namespace BugBattle {
+declare module "react-native-bugbattle-sdk" {
+  export namespace BugBattle {
     const NONE: "NONE";
     const SHAKE: "SHAKE";
     const THREE_FINGER_DOUBLE_TAB: "THREE_FINGER_DOUBLE_TAB";
-
-    function initWithToken(key: string, activationMethod: "NONE" | "SHAKE" | "THREE_FINGER_DOUBLE_TAB"): void;
+    function initWithToken(
+      key: string,
+      activationMethod: "NONE" | "SHAKE" | "THREE_FINGER_DOUBLE_TAB"
+    ): void;
     function startBugReporting(): void;
     function setCustomerEmail(email: string): void;
     function attachCustomData(customData: any): void;
     function enablePrivacyPolicy(enable: boolean): void;
     function setPrivacyPolicyUrl(privacyUrl: string): void;
     function setApiUrl(apiUrl: string): void;
+  }
+  export default BugBattle;
 }
