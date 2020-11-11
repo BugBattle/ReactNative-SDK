d9c49225b604eed3da9e983bf66b6cfd2d8b187a
niklasfessler <niklas@boehlerbrothers.como>
2020-11-11 18:39:24 +0100

    added android

--- android/src/main/java/com/reactlibrary/BugbattleSdkModule.java
+++ android/src/main/java/com/reactlibrary/BugbattleSdkModule.java
@@ -19,9 +19,69 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         return "BugbattleSdk";
     }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+     @ReactMethod
+    public void initialise(String sdkKey, String activationMethod) {
+        if(activationMethod.equals("SHAKE")) {
+            BugBattle.initialise(sdkKey, BugBattleActivationMethod.SHAKE, getCurrentActivity());
+        } else if(activationMethod.equals("THREE_FINGER_DOUBLE_TAB")) {
+            BugBattle.initialise(sdkKey, BugBattleActivationMethod.THREE_FINGER_DOUBLE_TAB, getCurrentActivity());
+        } else {
+            BugBattle.initialise(sdkKey, BugBattleActivationMethod.NONE, getCurrentActivity());
+        }
     }
+
+    @ReactMethod
+    public void startBugReporting() {
+        try {
+            BugBattle.startBugReporting();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @ReactMethod
+    public void startBugReporting(String base64) {
+        byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            BugBattle.startBugReporting(decodedByte);
+    }
+
+    @ReactMethod
+    public void trackStep(String type, String data) {
+        BugBattle.trackStep(type, data);
+    }
+
+    @ReactMethod
+    public void attachCustomData(JSONObject jsonObject) {
+        try {
+            BugBattle.attachCustomData(jsonObject);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @ReactMethod
+    public void setCustomerEmail(String email) {
+        BugBattle.setCustomerEmail(email);
+    }
+
+    @ReactMethod
+    public void enablePrivacyPolicy(boolean enable){
+        BugBattle.enablePrivacyPolicy(enable);
+    }
+
+    @ReactMethod
+    public void setPrivacyPolicyUrl(String privacyUrl){
+        BugBattle.setPrivacyPolicyUrl(privacyUrl);
+    }
+
+    @ReactMethod
+    public void setApiURL(String apiUrl){
+        try {
+            BugBattle.setApiURL(apiUrl);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
