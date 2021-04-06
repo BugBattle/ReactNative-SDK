679b32fe31e61fcf8826e0e77cadc1e99a809e1c
niklasfessler <niklas@boehlerbrothers.como>
2021-04-06 10:49:00 +0200

    fixed activity error, added comments

--- android/src/main/java/com/reactnativebugbattlesdk/BugbattleSdkModule.java
+++ android/src/main/java/com/reactnativebugbattlesdk/BugbattleSdkModule.java
@@ -1,5 +1,6 @@
 package com.reactnativebugbattlesdk;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
@@ -38,7 +39,12 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         return "BugbattleSdk";
     }
 
-
+    /**
+     * Initialize Bugbattle with SDK Key and Activation Methode
+     * For more information see https://docs.bugbattle.io
+     * @param sdkKey sdk key from your project
+     * @param activationMethods e.g. SHAKE
+     */
     @ReactMethod
     public void initializeMany(String sdkKey, ReadableArray activationMethods) {
         try {
@@ -65,50 +71,62 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
                     activationMethodsList.add(BugBattleActivationMethod.SCREENSHOT);
                 }
             }
-            BugBattle.initialise(sdkKey, activationMethodsList.toArray(new BugBattleActivationMethod[activationMethodsList.size()]), getReactApplicationContext().getCurrentActivity().getApplication());
+            Activity activity = getReactApplicationContext().getCurrentActivity();
+            if(activity != null ) {
+                BugBattle.initialise(sdkKey, activationMethodsList.toArray(new BugBattleActivationMethod[activationMethodsList.size()]), activity.getApplication());
+            }
 
-        }catch (Exception ex) {}
+        } catch (Exception ex) {
+        }
     }
 
+    /**
+     * Initialize Bugbattle with SDK Key and Activation Methode
+     * For more information see https://docs.bugbattle.io
+     * @param sdkKey sdk key from your project
+     * @param activationMethod e.g. SHAKE
+     */
     @ReactMethod
     public void initialize(String sdkKey, String activationMethod) {
+        System.out.println("HEY DU");
         try {
             getReactApplicationContext().addLifecycleEventListener(new LifecycleEventListener() {
                 @Override
                 public void onHostResume() {
-                    BugBattle.setApplicationType(APPLICATIONTYPE.REACTNATIVE);
-                    BugBattle.setBugWillBeSentCallback(new BugWillBeSentCallback() {
-                        @Override
-                        public void flowInvoced() {
-                            getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("bugWillBeSent", null);
-                        }
-                    });
-                    if (activationMethod.equals("SHAKE")) {
-                        BugBattle.initialise(sdkKey, BugBattleActivationMethod.SHAKE, getReactApplicationContext()
-                                .getCurrentActivity()
-                                .getApplication());
-                        BugBattle.setBugSentCallback(new BugSentCallback() {
-                            @Override
-                            public void close() {
-                                new java.util.Timer().schedule(
-                                        new java.util.TimerTask() {
-                                            @Override
-                                            public void run() {
-                                                showDevMenu();
-                                            }
-                                        },
-                                        500
-                                );
+                    try {
+                        Activity activity = getReactApplicationContext()
+                                .getCurrentActivity();
+                        if (activity != null) {
+                            BugBattle.setApplicationType(APPLICATIONTYPE.REACTNATIVE);
+                            BugBattle.setBugWillBeSentCallback(new BugWillBeSentCallback() {
+                                @Override
+                                public void flowInvoced() {
+                                    getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("bugWillBeSent", null);
+                                }
+                            });
+                            if (activationMethod.equals("SHAKE")) {
+                                BugBattle.initialise(sdkKey, BugBattleActivationMethod.SHAKE, activity.getApplication());
+                                BugBattle.setBugSentCallback(new BugSentCallback() {
+                                    @Override
+                                    public void close() {
+                                        new java.util.Timer().schedule(
+                                                new java.util.TimerTask() {
+                                                    @Override
+                                                    public void run() {
+                                                        showDevMenu();
+                                                    }
+                                                },
+                                                500
+                                        );
+                                    }
+                                });
+                            } else if (activationMethod.equals("SCREENSHOT")) {
+                                BugBattle.initialise(sdkKey, BugBattleActivationMethod.SCREENSHOT, activity.getApplication());
+                            } else {
+                                BugBattle.initialise(sdkKey, BugBattleActivationMethod.NONE, activity.getApplication());
                             }
-                        });
-                    } else if (activationMethod.equals("SCREENSHOT")) {
-                        BugBattle.initialise(sdkKey, BugBattleActivationMethod.SCREENSHOT, getReactApplicationContext()
-                                .getCurrentActivity()
-                                .getApplication());
-                    } else {
-                        BugBattle.initialise(sdkKey, BugBattleActivationMethod.NONE, getReactApplicationContext()
-                                .getCurrentActivity()
-                                .getApplication());
+                        }
+                    } catch (Exception ex) {
                     }
                 }
 
@@ -128,6 +146,9 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         }
     }
 
+    /**
+     * Show dev menu after shaking the phone.
+     */
     private void showDevMenu() {
         final ReactApplication application = (ReactApplication) getReactApplicationContext()
                 .getCurrentActivity()
@@ -150,6 +171,9 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         mainHandler.post(myRunnable);
     }
 
+    /**
+     * Start bug report manually by calling this function.
+     */
     @ReactMethod
     public void startBugReporting() {
         try {
@@ -180,6 +204,10 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         }
     }
 
+    /**
+     * Start bug report with a custom image.
+     * @param base64 Base64 encoded image
+     */
     @ReactMethod
     public void startBugReportingWithImage(String base64) {
         byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
@@ -187,6 +215,10 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         BugBattle.startBugReporting(decodedByte);
     }
 
+    /**
+     * Not implemented for now on Android
+     * @param jsonObject
+     */
     @ReactMethod
     public void attachCustomData(ReadableMap jsonObject) {
  /*      try {
@@ -196,21 +228,39 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         }*/
     }
 
+    /**
+     * Set default email address filled in the send dialog.
+     * @param email email you want to be set
+     */
     @ReactMethod
     public void setCustomerEmail(String email) {
         BugBattle.setCustomerEmail(email);
     }
 
+    /**
+     * Enable privacy policies shown in the send dialog. The policies url can be
+     * edited with {@methode setPrivacyPolicyUrl}
+     * @param enable if the value is true the privacy policies must be accepted by the user
+     */
     @ReactMethod
     public void enablePrivacyPolicy(boolean enable) {
         BugBattle.enablePrivacyPolicy(enable);
     }
 
+    /**
+     * Customize the url shown when clicked on the link. Highly recommended to add this url,
+     * when {@methode enablePrivacyPolicy} is set to true;
+     * @param privacyUrl URL to your privacy policies
+     */
     @ReactMethod
     public void setPrivacyPolicyUrl(String privacyUrl) {
         BugBattle.setPrivacyPolicyUrl(privacyUrl);
     }
 
+    /**
+     * Used for dedicated server. Set the url, where bugs are reported to.
+     * @param apiUrl Url to the dedicated server.
+     */
     @ReactMethod
     public void setApiUrl(String apiUrl) {
         try {
@@ -220,36 +270,49 @@ public class BugbattleSdkModule extends ReactContextBaseJavaModule {
         }
     }
 
+    /**
+     * Enable Replay function. Can be set to true, but is only available in certain plans.
+     * Please check https://www.bugbattle.io/pricing/
+     * @param enable enable replay function
+     */
     @ReactMethod
     public void enableReplays(boolean enable) {
         if (enable) {
             try {
                 Thread.sleep(1000);
                 BugBattle.enableReplay();
-            }catch (Exception ex) {
+            } catch (Exception ex) {
 
             }
         }
     }
 
+    /**
+     * Set language of the bugbattle dialog. Available languages are "en", "fr", "de", "it". You
+     * can override in the strings.xml and use your own language. For more informations see:
+     * https://developer.android.com/training/basics/supporting-devices/languages
+     * @param language available are "en", "fr", "de", "it"
+     */
     @ReactMethod
     public void setLanguage(String language) {
         BugBattle.setLanguage(language);
     }
 
+    /**
+     * Add network data to bugbattle.
+     * @param networkLog network log
+     */
     @ReactMethod
     public void attachNetworkLog(String networkLog) {
-        System.out.println(networkLog);
         try {
             JSONArray object = new JSONArray(networkLog);
             System.out.println(object);
             JSONObject networkObj = new JSONObject();
             networkObj.put("networkLogs", object);
             BugBattle.attachData(networkObj);
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             System.out.println(ex);
         }
-        //BugBattle.attachData(jsonObject);
     }
 
 }
